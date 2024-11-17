dd2718af14a6c73cdfdc560b82f3cb4161500e8d
kat14221 <ale123ferkatty@gmail.com>
2024-11-17 18:50:17 -0500

    yo

---
+++ src/main/java/com/example/demo/repository/UsuarioRepository.java
@@ -0,0 +1,12 @@
+package com.example.demo.repository;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.demo.entity.Usuario;
+
+import java.util.Optional;
+
+public interface UsuarioRepository extends JpaRepository<Usuario,Long> {
+    Optional<Usuario> findByUsername(String username);
+}
